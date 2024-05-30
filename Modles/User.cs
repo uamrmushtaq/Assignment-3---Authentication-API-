c75419bc0da0ad4e85119f2e7e31b7c8cfcedc4d
METRO <m.umarmushtaq6@gmail.com>
2024-05-30 03:36:09 -0700

    update

---
+++ Modles/User.cs
@@ -0,0 +1,10 @@
+﻿namespace OAuthLoginAPI.Modles
+{
+    public class User
+    {
+        public string Id { get; set; }
+        public string Username { get; set; }
+        public string Role { get; set; }
+        public string Scope { get; set; }
+    }
+}
